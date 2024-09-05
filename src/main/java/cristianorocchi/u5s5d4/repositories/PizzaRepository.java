0a56f076c295ed870b61ef6fe39687db618448ef
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-09-05 17:13:48 +0200

    aggiunta query

--- src/main/java/cristianorocchi/u5s5d4/repositories/PizzaRepository.java
+++ src/main/java/cristianorocchi/u5s5d4/repositories/PizzaRepository.java
@@ -4,8 +4,17 @@ package cristianorocchi.u5s5d4.repositories;
 
 import cristianorocchi.u5s5d4.entities.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PizzaRepository extends JpaRepository<Pizza, Long> {
+
+    List<Pizza> findByNome(String nome);
+
+
+    List<Pizza> findByCalorieGreaterThanEqual(int calorie);
 }
